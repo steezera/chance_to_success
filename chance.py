c9c0ff6b644c65e10ee9d6fbfe80fd11815a7278
jihunsong <songjihun1080@gmail.com>
2023-04-10 16:21:15 +0900

    add index of try

--- chance.py
+++ chance.py
@@ -3,7 +3,7 @@ import random
 total = 0
 for i in range(6):
 	randomvalue = random.random() * 100
-	print(randomvalue)
+	print(f"{i + 1} try : {randomvalue:.2f}")
 	total += randomvalue
 
 print(f"you will have {total/6:.2f}% chance to success.")
