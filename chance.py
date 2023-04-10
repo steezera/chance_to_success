b24fb7f5cd3b5826fe8105f1eef73f6d8503d810
jihunsong <songjihun1080@gmail.com>
2023-04-10 16:13:44 +0900

    add avg chance

--- chance.py
+++ chance.py
@@ -1,5 +1,9 @@
 import random
 
+total = 0
 for i in range(6):
-	randomvalue = random.random()
+	randomvalue = random.random() * 100
 	print(randomvalue)
+	total += randomvalue
+
+print(f"you will have {total/6:.2f}% chance to success.")
